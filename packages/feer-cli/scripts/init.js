bef2e48275a5232ea95c151d476dcca6cc5d9164
Likun <likun205743@sogou-inc.com>
2020-07-09 18:22:27 +0800

    :tada: Rename gitignore.

--- packages/feer-cli/scripts/init.js
+++ packages/feer-cli/scripts/init.js
@@ -8,15 +8,37 @@ const path = require('path');
 const fs = require('fs-extra');
 
 module.exports = (type) => {
-  const templatePath = path.resolve(__dirname, '..', `templates/${type}`);
-  const CWD = process.cwd();
+  //The template directory
+  const templatePath = path.resolve(__dirname, '..', `templates/${type}`); 
+  //The directory in which the process is currently working
+  const CWD = process.cwd(); 
   return new Promise((resolve, reject) => {
     fs.copy(templatePath, `${CWD}`, { overwrite: true }, (err) => {
       if (err) {
         reject(err);
         process.exit(1);
       }
-      console.log('\nInit template success!\n');
+
+      // Rename gitignore after the fact to prevent npm from renaming it to .gitignore
+      // See: https://github.com/npm/npm/issues/1862
+      try {
+        fs.moveSync(
+          path.join(CWD, 'gitignore'),
+          path.join(CWD, '.gitignore'),
+          []
+        );
+      } catch (error) {
+        // Append if there's already a `.gitignore` file there
+        if (error.code === 'EEXIST') {
+          const data = fs.readFileSync(path.join(CWD, 'gitignore'));
+          fs.appendFileSync(path.join(CWD, '.gitignore'), data);
+          fs.unlinkSync(path.join(CWD, 'gitignore'));
+        } else {
+          throw error;
+        }
+      }
+
+      console.log('\nInit template success! ✨\n');
       resolve();
     });
   });
