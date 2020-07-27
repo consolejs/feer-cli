36dd811d888f5662ff4ba9e19c350e31be807adf
Likun <likun205743@sogou-inc.com>
2020-07-27 12:21:51 +0800

    :rocket: Modify build configuration

--- packages/feer-cli/templates/feer-h5/config/build.js
+++ packages/feer-cli/templates/feer-h5/config/build.js
@@ -16,17 +16,29 @@ const directory = path.join(__dirname, "../views/");
 
 nunjucks.configure(path);
 
+// 输出html，压缩处理
+const miniFun = (res) => {
+  return minify(res, {
+    removeComments: true, //默认值false；是否去掉注释
+    collapseWhitespace: true, //默认值false；是否去掉空格
+    minifyJS: true, //是否压缩html里的js（使用uglify-js进行的压缩）
+    minifyCSS: true, //是否压缩html里的css（使用clean-css进行的压缩
+  })
+}
+
+//去字符串左右空格
+function trim(str) { //删除左右两端的空格
+  return str.replace(/(^\s*)|(\s*$)/g, "")
+}
+
 // 渲染方法
-function toRender(name) {
+function toRender(filesName) {
+  let name = trim(filesName);
+  if (name.length === 0) return;
   const res = nunjucks.render(directory + name + ".html", {
     version: Date.now()
-  });
-  fs.writeFileSync("./build/" + name + ".html", minify(res, {
-    removeComments: true,//默认值false；是否去掉注释
-    collapseWhitespace: true,//默认值false；是否去掉空格
-    minifyJS: true,//是否压缩html里的js（使用uglify-js进行的压缩）
-    minifyCSS: true//是否压缩html里的css（使用clean-css进行的压缩
-  }));
+  })
+  fs.writeFileSync("./build/" + name + ".html", miniFun(res));
 }
 
 // 待渲染文件名
