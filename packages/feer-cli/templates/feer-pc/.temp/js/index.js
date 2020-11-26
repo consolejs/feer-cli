e3fbeec2fd53e72effb076671d4d6be2e7b604b2
consolejs <https200@126.com>
2020-11-26 12:47:41 +0800

    :bug: Fixed some problems with Eslint.

--- packages/feer-cli/templates/feer-pc/.temp/js/index.js
+++ packages/feer-cli/templates/feer-pc/.temp/js/index.js
@@ -1,9 +1,9 @@
 
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':36892/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
+(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':46355/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
 (function () {
 	'use strict';
 
-
+	console.log('Feer!');
 
 }());
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7OyJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgJy4vc2Fzcy9pbmRleC5zY3NzJztcblxuY29uc29sZS5sb2coJ0ZlZXIhJyk7XG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7Ozs7Q0FFQUEsT0FBTyxDQUFDQyxHQUFSLENBQVksT0FBWjs7OzsifQ==
